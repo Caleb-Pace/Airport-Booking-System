9d48f782f4308894434544d09f8f7d4af9d2c54d
Caleb Pace <pacecaleb2004+Git@gmail.com>
2024-10-26 22:44:54 +1300

    chore(BookingController): add comments for improved code documentation

--- src/main/java/airportbookingsystem/BookingController.java
+++ src/main/java/airportbookingsystem/BookingController.java
@@ -6,25 +6,33 @@ public class BookingController {
     private static final String controllerName = "Booking Controller";
     private static boolean _isInitialised = false;
 
-    // TODO: Comment
+    /**
+     * Checks if the controller is initialised and ready for use.
+     *
+     * @return boolean True if the controller is initialized, otherwise false
+     */
     private static boolean isControllerReady() {
         if (_isInitialised)
             return true;
 
+        // Not initialised
         System.err.printf("%s: Not initialised!\n", controllerName);
         return false;
     }
 
-    // TODO: Comment
+    /**
+     * Initializes the controller by establishing a connection to the database.
+     */
     public static void init() {
-        FlightManager.load(); // Load in flight data
         BookingDBManager.getConnection();
 
         _isInitialised = true;
         System.out.printf("%s: Initialised!\n", controllerName);
     }
 
-    // TODO: Comment
+    /**
+     * Cleans up resources by closing the connection to the database.
+     */
     public static void cleanup() {
         BookingDBManager.close(); // Close connection
 
@@ -32,7 +40,13 @@ public class BookingController {
         System.out.printf("%s: Cleanup completed!\n", controllerName);
     }
 
-    // TODO: Comment
+    /**
+     * Creates and adds a booking to the database.
+     *
+     * @param passengerName Name of the passenger
+     * @param flight        The flight object for the booking
+     * @param seatNumber    The seat number for the booking
+     */
     public static void addBooking(String passengerName, Flight flight, String seatNumber) {
         // Get flight number
         String flightNumber = flight.getFlightNumber();
@@ -41,7 +55,13 @@ public class BookingController {
         addBooking(passengerName, flightNumber, seatNumber);
     }
 
-    // TODO: Comment
+    /**
+     * Creates and adds a booking to the database.
+     *
+     * @param passengerName Name of the passenger
+     * @param flightNumber  Flight number for the booking
+     * @param seatNumber    The seat number for the booking
+     */
     public static void addBooking(String passengerName, String flightNumber, String seatNumber) {
         if (!isControllerReady())
             return;
@@ -49,7 +69,11 @@ public class BookingController {
         BookingDBManager.add(getNextID(), passengerName, flightNumber, seatNumber);
     }
 
-    // TODO: Comment
+    /**
+     * Retrieves a list of booking IDs from the database.
+     *
+     * @return ArrayList<Integer> List of booking IDs
+     */
     public static ArrayList<Integer> getBookingIDs() {
         if (!isControllerReady())
         return null;
@@ -57,7 +81,12 @@ public class BookingController {
         return BookingDBManager.getAllIDs();
     }
     
-    // TODO: Comment
+    /**
+     * Retrieves a booking from the database by its ID.
+     *
+     * @param id Unique booking ID to search for
+     * @return Booking The booking object if found, or null if not found
+     */
     public static Booking getBooking(int id) {
         if (!isControllerReady())
             return null;
@@ -65,7 +94,11 @@ public class BookingController {
         return BookingDBManager.getByID(id);
     }
 
-    // TODO: Comment
+    /**
+     * Cancels a booking by removing it from the database.
+     *
+     * @param booking The booking object to be canceled
+     */
     public static void cancel(Booking booking) {
         if (!isControllerReady())
             return;
@@ -73,7 +106,11 @@ public class BookingController {
         BookingDBManager.removeByID(booking.getId());
     }
 
-    // TODO: Comment
+    /**
+     * Cancels a booking by removing it from the database using the booking ID.
+     *
+     * @param id Unique booking ID of the booking to be canceled
+     */
     public static void cancel(int id) {
         if (!isControllerReady())
             return;
@@ -81,7 +118,11 @@ public class BookingController {
         BookingDBManager.removeByID(id);
     }
 
-    // TODO: Comment
+    /**
+     * Retrieves the next available booking ID, which is the ID following the last used ID.
+     *
+     * @return int The next available booking ID
+     */
     private static int getNextID() {
         int id = 1000;
         ArrayList<Integer> ids = BookingDBManager.getAllIDs();
