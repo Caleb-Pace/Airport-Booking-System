34a192a47304ad4c440846e32a53c0dac63ee59c
Caleb Pace <pacecaleb2004+Git@gmail.com>
2024-10-26 13:42:39 +1300

    feat(Controller): add addBooking method with safety check for initialization

--- src/main/java/airportbookingsystem/BookingController.java
+++ src/main/java/airportbookingsystem/BookingController.java
@@ -1,16 +1,39 @@
 package airportbookingsystem;
 
 public class BookingController {
+    private static boolean _isInitialised = false;
+
+    // TODO: Comment
     public static void init() {
         FlightManager.load(); // Load in flight data
         BookingDBManager.getConnection();
-        
+
+        _isInitialised = true;
         System.out.println("Controller: Initialised!");
     }
 
+    // TODO: Comment
     public static void cleanup() {
         BookingDBManager.close(); // Close connection
 
+        _isInitialised = false;
         System.out.println("Controller: Cleanup completed!");
+    }
+
+    // TODO: Comment
+    public static void addBooking(int id, String passengerName , String flightNumber, String seatNumber) {
+        if (!isControllerReady())
+            return;
+        
+        BookingDBManager.add(id, passengerName, flightNumber, seatNumber);
+    }
+
+    // TODO: Comment
+    private static boolean isControllerReady() {
+        if (_isInitialised)
+            return true;
+
+        System.err.println("Controller: Not initialised!");
+        return false;
     }
 }
